ea009288a51fda993ccbb6fb92a5397ef5e9ae70
Ahmedemm <falkonne97@gmail.com>
2023-09-29 09:34:06 +0200

    debut d'histoire

---
+++ README.MK
@@ -0,0 +1,4 @@
+#La foret farfeleu 
+Dans une forêt enchantée, les arbres se mirent soudainement à danser, leurs branches entrelacées formant une étrange chorégraphie. Les lapins et les écureuils les observèrent avec étonnement, se demandant s'ils étaient tombés dans une autre dimension. C'était le jour où la forêt décida de montrer son côté farfelu.
+Le chef des arbres, un vieux chêne sage, annonça qu'ils avaient décidé de lancer un concours de danse pour tous les habitants de la forêt. Les animaux se préparèrent rapidement, répétant des mouvements extravagants et des pas de danse inattendus. La compétition s'annonçait comme le spectacle le plus farfelu jamais vu dans la forêt enchantée.
+!(https://media.tenor.com/WJOWXkfo6cMAAAAM/tribe-oak.gif)
